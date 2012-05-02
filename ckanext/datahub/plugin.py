cbd8efea472c49616ed45f6e161b5ae39f0bbd03
Toby <toby.junk@gmail.com>
2012-05-02 14:35:14 +0100

    add docstring

--- ckanext/datahub/plugin.py
+++ ckanext/datahub/plugin.py
@@ -2,6 +2,10 @@ import ckan.plugins as p
 
 
 class DataHub(p.SingletonPlugin):
+    '''
+    This plugin provides customisation specific to datahub.
+    '''
+
     p.implements(p.IConfigurer)
 
     def update_config(self, config):
