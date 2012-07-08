4ffae810f92511954a7f8de31bfa9b72e3c1caff
Ian Murray <ian.c.murray@gmail.com>
2012-07-08 13:07:15 +0100

    [#2550] Clean up of the models.

--- ckanext/datahub/models.py
+++ ckanext/datahub/models.py
@@ -14,11 +14,14 @@ _log = logging.getLogger(__name__)
 
 # See ``_define_datahub_tables()`` for the details of these tables.
 _datahub_migration_table = None
-_user_account_levels_table = None
+_datahub_paid_service_table = None
+_datahub_paid_service_to_users_table = None
 
 _DATAHUB_TABLES_VERSION = 1
 
 def setup():
+    '''Call this to ensure datahub-specific db tables exist and up to date'''
+    
     # Setup table definitions in memory if not done so already.
     if _datahub_migration_table is None:
         _define_datahub_tables()
@@ -26,13 +29,8 @@ def setup():
     if model.repo.are_tables_created():
 
         if not _datahub_migration_table.exists():
-            import pdb;pdb.set_trace()
             _log.debug('No datahub-specific tables exist.')
-            _datahub_migration_table.create()
-
-            m = _MigrationVersion(current_version=0)
-            m.save()
-            _log.debug('Created datahub_migration table')
+            _run_migration_step(0)
 
         for version in range(_current_migration_version(),
                              _DATAHUB_TABLES_VERSION):
@@ -40,10 +38,21 @@ def setup():
     else:
         _log.debug('Datahub-specific table creation deferred.')
 
+### Domain Model Definitions ###
+
+class _MigrationVersion(model.domain_object.DomainObject):
+    pass
+
+class PaidService(model.domain_object.DomainObject):
+    pass
+
+### Private module-level helper functions ###
+
 def _define_datahub_tables():
     '''Define the datahub tables in memory.'''
 
-    global _user_account_levels_table
+    global _datahub_paid_service_table
+    global _datahub_paid_service_to_users_table
     global _datahub_migration_table
 
     # This table is used to track what migration step the datahub-specific
@@ -56,17 +65,34 @@ def _define_datahub_tables():
                    primary_key=True),
     )
 
-    # This table tracks which users have an account level above an unpaid
-    # account level.  And which level that is.  It is assumed that there is a
-    # 1-1 mapping between users and paid accounts.
-    _user_account_levels_table = sql.Table('user_account_levels', db.metadata,
+    # This table is a table of paid-for service levels for the datahub.
+    # It's meant to be a linear hierarchy of levels, eg.
+    #
+    # basic account -> small business account -> enterprise account
+    _datahub_paid_service_table = sql.Table('datahub_paid_services', db.metadata,
+        sql.Column('id',
+                   sql.types.UnicodeText,
+                   default = model.types.make_uuid,
+                   nullable = False,
+                   primary_key = True),
+        sql.Column('name',
+                   sql.types.UnicodeText,
+                   nullable = False),
+    )
+
+    # This table tracks which users have a paid-for service account, and
+    # which level that account is.  Each user can have up to one paid-for
+    # account.
+    _datahub_paid_service_to_users_table = sql.Table(
+        'datahub_paid_service_to_users', db.metadata,
         sql.Column('user_id',
                    sql.types.UnicodeText,
                    sql.ForeignKey('user.id'),
-                   primary_key=True),
-        sql.Column('account_level',
+                   primary_key = True),
+        sql.Column('paid_service_id',
                    sql.types.UnicodeText,
-                   nullable=False),
+                   sql.ForeignKey('datahub_paid_services.id'),
+                   nullable = False),
     )
 
     # Define the mappings between tables and domain objects.
@@ -75,17 +101,32 @@ def _define_datahub_tables():
         _datahub_migration_table,
     )
 
+    # The mapping between a PaidService and Users is through the
+    # `_datahub_paid_service_to_users_table` table, despite being a 1-many
+    # relationship from PaidService to User.  The reason for this is that we
+    # don't want to alter the User table, as it may be altered independantly
+    # by CKAN core library in the future.  And by normalizing out the
+    # PaidService table, we can store further information against each
+    # PaidService if we need to.
+    #
+    # paid_service_instance.users is all the Users that have paid for the
+    # given paid service.  It is lazily loaded.
+    #
+    # user_instance.paid_service is the PaidService that the User belongs to,
+    # or None if the User has not paid for any service.  Again, this is loaded
+    # lazily.
     db.mapper(
-        PaidAccount,
-        _user_account_levels_table,
+        PaidService,
+        _datahub_paid_service_table,
         properties = {
-            'user': sql_orm.relation(
+            'users': sql_orm.relation(
                 model.User,
-                backref=sql_orm.backref(
-                    'paid_account',
-                    uselist=False,
-                    lazy='joined'),
-                lazy=True,
+                secondary = _datahub_paid_service_to_users_table,
+                backref = sql_orm.backref(
+                    'paid_service',
+                    uselist = False, # user.paid_service is singular.
+                    lazy = True),
+                lazy = True,
             ),
         }
     )
@@ -95,19 +136,30 @@ def _current_migration_version():
                      .scalar() or 0
 
 def _run_migration_step(version):
-    _log.debug('Running migration step %d', version)
+    _log.debug('Running datahub migration step %d', version)
+    if version == 0:
+        _migration_step_0()
     if version == 1:
         _migration_step_1()
     else:
         raise Exception('Cannot run datahub migration step %d' % version)
+    _set_migration_version(version)
+
+def _set_migration_version(version):
+    assert version <= _DATAHUB_TABLES_VERSION
+    m = _MigrationVersion(current_version=version)
+    m.save()
+    _log.debug('Set datahub migration version to %d', version)
+
+### Migration steps ###
+
+def _migration_step_0():
+    _log.debug('Creating datahub_migration table')
+    _datahub_migration_table.create()
 
 def _migration_step_1():
-    _log.debug('Creating user_paid_service table.')
-    _user_account_levels_table.create()
-
-class _MigrationVersion(model.domain_object.DomainObject):
-    pass
-
-class PaidAccount(model.domain_object.DomainObject):
-    pass
+    _log.debug('Creating datahub_paid_services table.')
+    _datahub_paid_service_table.create()
+    _log.debug('Creating datahub_paid_service_to_users table.')
+    _datahub_paid_service_to_users_table.create()
 
