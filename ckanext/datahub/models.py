ba34cc256b0ed5f88ebc61629379e9cf1d29f881
Ian Murray <ian.c.murray@gmail.com>
2012-07-13 15:35:24 +0100

    [#2550] pep8 models.py

--- ckanext/datahub/models.py
+++ ckanext/datahub/models.py
@@ -19,6 +19,7 @@ _datahub_paid_service_to_users_table = None
 
 _DATAHUB_TABLES_VERSION = 1
 
+
 def setup():
     '''Call this to ensure datahub-specific db tables exist and up to date'''
     # Setup table definitions in memory if not done so already.
@@ -33,19 +34,22 @@ def setup():
 
         for version in range(_current_migration_version(),
                              _DATAHUB_TABLES_VERSION):
-            _run_migration_step(version+1)
+            _run_migration_step(version + 1)
     else:
         _log.info('Datahub-specific table creation deferred.')
 
 ### Domain Model Definitions ###
 
+
 class _MigrationVersion(model.domain_object.DomainObject):
     pass
+
 
 class PaidService(model.domain_object.DomainObject):
     pass
 
 ### Private module-level helper functions ###
+
 
 def _define_datahub_tables():
     '''Define the datahub tables in memory.'''
@@ -56,7 +60,8 @@ def _define_datahub_tables():
 
     # This table is used to track what migration step the datahub-specific
     # tables are currently at.  It's intended to only have a single row.
-    _datahub_migration_table = sql.Table('datahub_migrations', db.metadata,
+    _datahub_migration_table = sql.Table(
+        'datahub_migrations', db.metadata,
         sql.Column('current_version',
                    sql.types.Integer,
                    nullable=False,
@@ -68,15 +73,16 @@ def _define_datahub_tables():
     # It's meant to be a linear hierarchy of levels, eg.
     #
     # basic account -> small business account -> enterprise account
-    _datahub_paid_service_table = sql.Table('datahub_paid_services', db.metadata,
+    _datahub_paid_service_table = sql.Table(
+        'datahub_paid_services', db.metadata,
         sql.Column('id',
                    sql.types.UnicodeText,
-                   default = model.types.make_uuid,
-                   nullable = False,
-                   primary_key = True),
+                   default=model.types.make_uuid,
+                   nullable=False,
+                   primary_key=True),
         sql.Column('name',
                    sql.types.UnicodeText,
-                   nullable = False),
+                   nullable=False),
     )
 
     # This table tracks which users have a paid-for service account, and
@@ -87,11 +93,11 @@ def _define_datahub_tables():
         sql.Column('user_id',
                    sql.types.UnicodeText,
                    sql.ForeignKey('user.id'),
-                   primary_key = True),
+                   primary_key=True),
         sql.Column('paid_service_id',
                    sql.types.UnicodeText,
                    sql.ForeignKey('datahub_paid_services.id'),
-                   nullable = False),
+                   nullable=False),
     )
 
     # Define the mappings between tables and domain objects.
@@ -117,22 +123,24 @@ def _define_datahub_tables():
     db.mapper(
         PaidService,
         _datahub_paid_service_table,
-        properties = {
+        properties={
             'users': sql_orm.relation(
                 model.User,
-                secondary = _datahub_paid_service_to_users_table,
-                backref = sql_orm.backref(
+                secondary=_datahub_paid_service_to_users_table,
+                backref=sql_orm.backref(
                     'paid_service',
-                    uselist = False, # user.paid_service is singular.
-                    lazy = True),
-                lazy = True,
+                    uselist=False,
+                    lazy=True),
+                lazy=True,
             ),
         }
     )
 
+
 def _current_migration_version():
     return db.Session.query(sql_func.max(_MigrationVersion.current_version)) \
                      .scalar() or 0
+
 
 def _run_migration_step(version):
     _log.info('Running datahub migration step %d', version)
@@ -144,6 +152,7 @@ def _run_migration_step(version):
         raise Exception('Cannot run datahub migration step %d' % version)
     _set_migration_version(version)
 
+
 def _set_migration_version(version):
     assert version <= _DATAHUB_TABLES_VERSION
     m = _MigrationVersion(current_version=version)
@@ -152,12 +161,15 @@ def _set_migration_version(version):
 
 ### Migration steps ###
 
+
 def _migration_step_0():
     _create_table(_datahub_migration_table)
+
 
 def _migration_step_1():
     _create_table(_datahub_paid_service_table)
     _create_table(_datahub_paid_service_to_users_table)
+
 
 def _create_table(t):
     if not t.exists():
