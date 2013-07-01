345850d2bb02ea28018ccbbfc7fa15781c27628d
John Glover <j@johnglover.net>
2013-07-01 12:00:49 +0200

    Add auth functions and update main template for read-only mode.

--- ckanext/datahub/plugin.py
+++ ckanext/datahub/plugin.py
@@ -1,13 +1,40 @@
 import ckan.plugins as p
 
 
+# auth functions for read-only mode
+#
+# disable create/update of packages and groups for all users
+
+def package_update(context, data_dict):
+    return {'success': False}
+
+
+def package_create(context, data_dict):
+    return {'success': False}
+
+
+def group_update(context, data_dict):
+    return {'success': False}
+
+
+def group_create(context, data_dict):
+    return {'success': False}
+
+
 class DataHub(p.SingletonPlugin):
     '''
     This plugin provides customisation specific to datahub.
     '''
 
     p.implements(p.IConfigurer)
+    p.implements(p.IAuthFunctions)
 
     def update_config(self, config):
         p.toolkit.add_template_directory(config, 'templates')
         p.toolkit.add_public_directory(config, 'public')
+
+    def get_auth_functions(self):
+        return {'package_create': package_create,
+                'package_update': package_update,
+                'group_create': group_create,
+                'group_update': group_update}
