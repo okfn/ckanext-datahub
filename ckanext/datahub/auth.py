3313f89bd36c8681c670d9bf9b3bc759bb312e7f
kindly <kindly@gmail.com>
2013-07-25 12:57:48 +0100

    remove traslations

--- ckanext/datahub/auth.py
+++ ckanext/datahub/auth.py
@@ -1,6 +1,5 @@
 import ckan.model as model
 import ckan.new_authz as new_authz
-from ckan.common import _
 
 
 def package_delete(context, data_dict):
@@ -21,7 +20,7 @@ def resource_delete(context, data_dict):
 def file_upload(context, data_dict=None):
     user = context['user']
     if not new_authz.auth_is_registered_user():
-        return {'success': False, 'msg': _('User %s not authorized to create packages') % user}
+        return {'success': False, 'msg': ('User %s not authorized to create packages') % user}
     if not new_authz.has_user_permission_for_some_org(user, 'create_dataset'):
-        return {'success': False, 'msg': _('User %s not authorized to upload data') % user}
+        return {'success': False, 'msg': ('User %s not authorized to upload data') % user}
     return {'success': True}
