4d0a0f884106172fe5b3fdfd278836a7a6e0cbfb
Alex Sadleir <maxious@lambdacomplex.org>
2013-12-23 19:16:16 +1100

    Add revision/dataset only and dry run command line options

--- ckanext/datahub/commands/purge.py
+++ ckanext/datahub/commands/purge.py
@@ -11,19 +11,52 @@ class PurgeCommand(p.toolkit.CkanCommand):
     '''Purge deleted datasets and revisions
 
     Usage::
-        paster purge
+        paster purge revisions Purge revisions marked for deletion
+        paster purge datasets  Purge datasets marked for deletion
+        paster purge all       Purge datasets and revisions marked for deletion
+        paster purge list      Show datasets and revisions marked for deletion
 
     '''
     summary = __doc__.split('\n')[0]
     usage = __doc__
-    max_args = 0
-    min_args = 0
+    max_args = 1
+    min_args = 1
 
     def command(self):
-        self._load_config()
-        self.purge()
+        if not self.args or self.args[0] in ['--help', '-h', 'help'] or \
+                not len(self.args) in [1, 2]:
+            print self.usage
+            return
 
-    def purge_revs(self, revs_to_purge):
+        cmd = self.args[0]
+        self._load_config()
+
+        if cmd == 'revisions':
+            self.purge_revs()
+            print (_('Purge complete'))
+        elif cmd == 'datasets':
+            self.purge_packages()
+            print (_('Purge complete'))
+        elif cmd == 'all':
+            self.purge_revs()
+            self.purge_packages()
+            print (_('Purge complete'))
+        elif cmd == 'list':
+            print "Deleted Revisions:"
+            for rev in self.get_deleted_revisions():
+                print "Revision %s" % (rev.id)
+            print "Deleted Packages:"
+            for pkg in self.get_deleted_packages():
+                print "Package %s (%s)" % (pkg.id, pkg.name)
+                for x in pkg.all_related_revisions:
+                    print "Revision %s for package %s" % (
+                        x[0].id, pkg.id)
+        else:
+            print 'Error: Command "{0}" not recognized'.format(cmd)
+            print
+            print self.usage
+
+    def do_purge_revs(self, revs_to_purge):
         import ckan.model as model
         for id in revs_to_purge:
             revision = model.Session.query(model.Revision).get(id)
@@ -32,22 +65,31 @@ class PurgeCommand(p.toolkit.CkanCommand):
             except Exception, inst:
                 msg = _('Problem purging revision %s: %s') % (id, inst)
 
-    def purge(self):
+    def get_deleted_revisions(self):
         import ckan.model as model
-        deleted_revisions = model.Session.query(
+        return model.Session.query(
             model.Revision).filter_by(state=model.State.DELETED)
-        deleted_packages = model.Session.query(
+
+    def get_deleted_packages(self):
+        import ckan.model as model
+        return model.Session.query(
             model.Package).filter_by(state=model.State.DELETED)
+
+    def purge_revs(self):
         print "Processing deleted revisions"
-        self.purge_revs([rev.id for rev in deleted_revisions])
-        revs_to_purge = []
-        for pkg in deleted_packages:
-            print "Processing package %s" % (pkg.id)
-            revisions = [x[0] for x in pkg.all_related_revisions]
+        self.do_purge_revs([rev.id for rev in self.get_deleted_revisions()])
+
+    def purge_packages(self):
+        import ckan.model as model
+        for pkg in self.get_deleted_packages():
+            print "Processing package %s (%s)" % (pkg.id, pkg.name)
+            revisions = [x[0] for x in model.Session.query(
+                model.Package).get(pkg.id).all_related_revisions]
             problem = False
             for r in revisions:
+                print "Processing revision %s for package %s" % (r.id, pkg.id)
                 affected_pkgs = set(
-                    r.packages).difference(set(deleted_packages))
+                    r.packages).difference(set(self.get_deleted_packages()))
                 if affected_pkgs:
                     msg = _('Cannot purge package %s as '
                             'associated revision %s includes '
@@ -58,6 +100,4 @@ class PurgeCommand(p.toolkit.CkanCommand):
             if not problem:
                 print "Deleting %d revisions for package %s" % (
                     len(revisions), pkg.id)
-                self.purge_revs([r.id for r in revisions])
-            model.Session.remove()
-        print (_('Purge complete'))
+                self.do_purge_revs([r.id for r in revisions])
